8d0abe8645058c4e3024f44a585776201454a05b
Maja K <115983133+vollkornbiene@users.noreply.github.com>
2023-01-14 13:05:27 +0100

    add canvas for counter, score is now visible during gameplay

--- Assets/Scripts/CollectedCounter.cs
+++ Assets/Scripts/CollectedCounter.cs
@@ -1,19 +1,30 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class CollectedCounter : MonoBehaviour
 {
+    // for ingredients
     public int IngredientCount;
     public int CurrentIngredients;
 
+    private Text ingredientsUI;
+    private string ingredientsText = "Ingredients collected:  ";
+
+    // for trash
     public int TrashCount;
     public int CurrentTrash;
+
+    private Text trashUI;
+    private string trashText = "Trash collected:  ";
+
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        ingredientsUI = GameObject.Find("IngredientsText").GetComponent<Text>();
+        trashUI = GameObject.Find("TrashText").GetComponent<Text>();
     }
 
     // Update is called once per frame
@@ -25,10 +36,12 @@ public class CollectedCounter : MonoBehaviour
     public void UpdateIngredients(int IngredientCount)
     {
         CurrentIngredients += IngredientCount;
+        ingredientsUI.text = ingredientsText + CurrentIngredients.ToString();
     }
 
     public void UpdateTrash(int TrashCount)
     {
         CurrentTrash += TrashCount;
+        trashUI.text = trashText + CurrentTrash.ToString();
     }
 }
