f0919c8eb115a7ab92296527b35b321f17bcee4e
Maja K <115983133+vollkornbiene@users.noreply.github.com>
2023-01-14 12:30:36 +0100

    trash and ingredients cannot be collected or moved when they hit the pizza

--- Assets/Scripts/CollectingIngredient.cs
+++ Assets/Scripts/CollectingIngredient.cs
@@ -12,16 +12,31 @@ public class CollectingIngredient : MonoBehaviour
 
     public int IngredientCount = 1;
 
+    // for on pizza
+    private bool isOnPizza = false;
+
+    private Rigidbody _rb;
+
+
     // Start is called before the first frame update
     void Start()
     {
         Basket = GameObject.FindWithTag("Collecter");
         Counter = GameObject.Find("CollectedCounter").GetComponent<CollectedCounter>();
+        _rb = GetComponent<Rigidbody>();
     }
 
     private void OnCollisionEnter(Collision collision)
     {
-        if (collision.gameObject.CompareTag("Collecter"))
+        if (collision.gameObject.CompareTag("Pizza"))
+        {
+            isOnPizza = true;
+
+            //Freeze all positions
+            _rb.constraints = RigidbodyConstraints.FreezePosition;
+        }
+
+        if (collision.gameObject.CompareTag("Collecter") && !isOnPizza)
         {
             // print to console
             print("Rat collected Ingredient");
